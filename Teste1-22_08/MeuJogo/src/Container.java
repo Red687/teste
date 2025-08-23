f4bb3f035a3ff84b370f9acfbc627b58bdc7eede
Red687 <chaosgabriel7@gmail.com>
2025-08-22 22:13:00 -0300

    Jogo

---
+++ Teste1-22_08/MeuJogo/src/Container.java
@@ -0,0 +1,19 @@
+
+import javax.swing.JFrame;
+
+public class Container extends JFrame {
+    
+    public Container() {
+        setTitle("Meu Jogo");
+        setSize(1024, 728);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null); 
+        this.setResizable(false); 
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Container();
+    }
+}
+ 
---
+++ Teste1-22_08/MeuJogo/src/Fase.java
@@ -0,0 +1,16 @@
+
+import java.awt.Image;
+import javax.swing.ImageIcon;
+
+public class Fase {
+    
+    private Image fundo;
+
+    public Fase(Image fundo) {
+        ImageIcon referencia = new ImageIcon("res\\images.png");
+        fundo = referencia.getImage();    
+        
+        
+    
+    }
+}
